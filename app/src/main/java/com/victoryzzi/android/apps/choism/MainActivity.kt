5837a232b10dfd4e6c97604a7f42d6cb4657503b
최승명 <dhdndi2@gmail.com>
2020-05-26 21:51:02 +0900

    딥링크 주석 해제

--- app/src/main/java/com/victoryzzi/android/apps/choism/MainActivity.kt
+++ app/src/main/java/com/victoryzzi/android/apps/choism/MainActivity.kt
@@ -19,34 +19,21 @@ class MainActivity : AppCompatActivity() {
             DataBindingUtil.setContentView(this, R.layout.activity_main)
         mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
-        mainViewModel.apply {
-            requestOwner("ChoiSeungMyung")
-            requestRepo("ChoiSeungMyung")
+        intent.data?.let {
+            intent.dataString?.let { userName ->
+                mainViewModel.apply {
+                    requestOwner(userName)
+                    requestRepo(userName)
 
-            responseOwnerLiveData.observe(this@MainActivity, Observer {
-                binding.owner = it
-            })
+                    responseOwnerLiveData.observe(this@MainActivity, Observer {
+                        binding.owner = it
+                    })
 
-            responseRepoLiveData.observe(this@MainActivity, Observer {
-                repoListAdapter.submitList(it)
-            })
+                    responseRepoLiveData.observe(this@MainActivity, Observer {
+                        repoListAdapter.submitList(it)
+                    })
+                }
+            }
         }
-
-//        intent.data?.let {
-//            intent.dataString?.let { userName ->
-//                mainViewModel.apply {
-//                    requestOwner(userName)
-//                    requestRepo(userName)
-//
-//                    responseOwnerLiveData.observe(this@MainActivity, Observer {
-//                        binding.owner = it
-//                    })
-//
-//                    responseRepoLiveData.observe(this@MainActivity, Observer {
-//                        repoListAdapter.submitList(it)
-//                    })
-//                }
-//            }
-//        }
     }
 }
