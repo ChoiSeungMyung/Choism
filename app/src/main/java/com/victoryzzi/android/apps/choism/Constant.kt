3506034536bae31d356dbb483ccb1a447722937d
최승명 <dhdndi2@gmail.com>
2020-05-26 21:43:19 +0900

    m

--- app/src/main/java/com/victoryzzi/android/apps/choism/Constant.kt
+++ app/src/main/java/com/victoryzzi/android/apps/choism/Constant.kt
@@ -1,5 +1,3 @@
 package com.victoryzzi.android.apps.choism
 
-const val BASE_URL = "https://api.github.com/users"
-const val OWNER_URL = ":username"
-const val REPO_URL = ":username/repos"
+const val BASE_URL = "https://api.github.com/users/"
